e0f96b0a0ebed28c24c56b2977ff9ecfe87411fb
Dirk Küver-Christen <dirk@kuever.de>
2021-03-24 22:07:42 +0100

    fullscript

---
+++ piholer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Das Skript nimmt die url_list.txt Datei und erstellt eine neue
+# Gesamtdatei "pihole_list_raw.txt, die allerdings noch nicht bereinigt wurde auf:
+# - Dopplungen
+# - Kommentare
+
+# zunächst eine bestehende (ältere) Datei löschen
+rm pihole_list_raw.txt
+rm pihole_final.txt
+
+# Einlesen der URL Liste und in eine neue Liste zusammenfügen
+cat url_list.txt | while read url; do
+     echo "$url"
+     curl "$url" >> pihole_list_raw.txt;
+done
+sort -u pihole_list_raw.txt  > pihole_final.txt
